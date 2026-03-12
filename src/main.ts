d28c2df968d28dedf155fdbb0a02f871b2715e0c
adletb7 <adlet.baiteliyev@gmail.com>
2026-03-12 17:29:48 +0500

    feat: add loader and error handler to main page

--- src/main.ts
+++ src/main.ts
@@ -3,15 +3,41 @@ import { renderFeedback } from "./render/renderFeedback"
 import { renderPets } from "./render/renderPets"
 
 async function init(): Promise<void> {
+  loadPets()
+  loadFeedback()
+}
+
+async function loadPets(): Promise<void> {
+  const container = document.getElementById("petsContainer")
+
   try {
     const petsResponse = await getPets()
     renderPets(petsResponse.data)
+  } catch (error) {
+    if (container) {
+      container.innerHTML = `
+        <div class="api-error">
+          Something went wrong. Please, refresh the page
+        </div>
+      `
+    }
+  }
+}
 
+async function loadFeedback(): Promise<void> {
+  const container = document.getElementById("testimonialsContainer")
+
+  try {
     const feedbackResponse = await getFeedback()
     renderFeedback(feedbackResponse.data)
-
   } catch (error) {
-    console.error("Error loading data:", error)
+    if (container) {
+      container.innerHTML = `
+        <div class="api-error">
+          Something went wrong. Please, refresh the page
+        </div>
+      `
+    }
   }
 }
 
