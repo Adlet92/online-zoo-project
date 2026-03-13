73205708cead8ca7dba5faeb3490469408b847b6
adletb7 <adlet.baiteliyev@gmail.com>
2026-03-13 16:02:47 +0500

    feat: add images for zoo cameras until id 14

--- src/data/zooMedia.ts
+++ src/data/zooMedia.ts
@@ -53,67 +53,67 @@ export const zooMedia: Record<number, ZooMedia> = {
     ]
   },
   7: {
-    main: "../../assets/images/zoos/eagle_big.png",
+    main: "../../assets/images/zoos/african_lion_big.png",
     thumbnails: [
-      "../../assets/images/zoos/eagles_cam1.png",
-      "../../assets/images/zoos/eagles_cam2.png",
-      "../../assets/images/zoos/eagles_cam3.png"
+      "../../assets/images/zoos/african_lion_cam1.png",
+      "../../assets/images/zoos/african_lion_cam2.png",
+      "../../assets/images/zoos/african_lion_cam3.png"
     ]
   },
   8: {
-    main: "../../assets/images/zoos/eagle_big.png",
+    main: "../../assets/images/zoos/sumatran_tiger_big.png",
     thumbnails: [
-      "../../assets/images/zoos/eagles_cam1.png",
-      "../../assets/images/zoos/eagles_cam2.png",
-      "../../assets/images/zoos/eagles_cam3.png"
+      "../../assets/images/zoos/sumatran_tiger_cam1.png",
+      "../../assets/images/zoos/sumatran_tiger_cam2.png",
+      "../../assets/images/zoos/sumatran_tiger_cam3.png"
     ]
   },
   9: {
-    main: "../../assets/images/zoos/eagle_big.png",
+    main: "../../assets/images/zoos/red_panda_big.png",
     thumbnails: [
-      "../../assets/images/zoos/eagles_cam1.png",
-      "../../assets/images/zoos/eagles_cam2.png",
-      "../../assets/images/zoos/eagles_cam3.png"
+      "../../assets/images/zoos/red_panda_cam1.png",
+      "../../assets/images/zoos/red_panda_cam2.png",
+      "../../assets/images/zoos/red_panda_cam3.png"
     ]
   },
   10: {
-    main: "../../assets/images/zoos/eagle_big.png",
+    main: "../../assets/images/zoos/mountain_gorilla_big.png",
     thumbnails: [
-      "../../assets/images/zoos/eagles_cam1.png",
-      "../../assets/images/zoos/eagles_cam2.png",
-      "../../assets/images/zoos/eagles_cam3.png"
+      "../../assets/images/zoos/mountain_gorilla_cam1.png",
+      "../../assets/images/zoos/mountain_gorilla_cam2.png",
+      "../../assets/images/zoos/mountain_gorilla_cam3.png"
     ]
   },
   11: {
-    main: "../../assets/images/zoos/eagle_big.png",
+    main: "../../assets/images/zoos/african_elephant_big.png",
     thumbnails: [
-      "../../assets/images/zoos/eagles_cam1.png",
-      "../../assets/images/zoos/eagles_cam2.png",
-      "../../assets/images/zoos/eagles_cam3.png"
+      "../../assets/images/zoos/african_elephant_cam1.png",
+      "../../assets/images/zoos/african_elephant_cam2.png",
+      "../../assets/images/zoos/african_elephant_cam3.png"
     ]
   },
   12: {
-    main: "../../assets/images/zoos/eagle_big.png",
+    main: "../../assets/images/zoos/sea_otter_big.png",
     thumbnails: [
-      "../../assets/images/zoos/eagles_cam1.png",
-      "../../assets/images/zoos/eagles_cam2.png",
-      "../../assets/images/zoos/eagles_cam3.png"
+      "../../assets/images/zoos/sea_otter_cam1.png",
+      "../../assets/images/zoos/sea_otter_cam2.png",
+      "../../assets/images/zoos/sea_otter_cam3.png"
     ]
   },
   13: {
-    main: "../../assets/images/zoos/eagle_big.png",
+    main: "../../assets/images/zoos/bengal_tiger_big.png",
     thumbnails: [
-      "../../assets/images/zoos/eagles_cam1.png",
-      "../../assets/images/zoos/eagles_cam2.png",
-      "../../assets/images/zoos/eagles_cam3.png"
+      "../../assets/images/zoos/bengal_tiger_cam1.png",
+      "../../assets/images/zoos/bengal_tiger_cam2.png",
+      "../../assets/images/zoos/bengal_tiger_cam3.png"
     ]
   },
   14: {
-    main: "../../assets/images/zoos/eagle_big.png",
+    main: "../../assets/images/zoos/gray_wolf_big.png",
     thumbnails: [
-      "../../assets/images/zoos/eagles_cam1.png",
-      "../../assets/images/zoos/eagles_cam2.png",
-      "../../assets/images/zoos/eagles_cam3.png"
+      "../../assets/images/zoos/gray_wolf_cam1.png",
+      "../../assets/images/zoos/gray_wolf_cam2.png",
+      "../../assets/images/zoos/gray_wolf_cam3.png"
     ]
   },
   15: {
