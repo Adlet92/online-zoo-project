109142d2304ab4eed8c16af594fbdbc0af836e4c
adletb7 <adlet.baiteliyev@gmail.com>
2026-03-15 22:27:07 +0500

    fix: fix pages for netlify

--- vite.config.ts
+++ vite.config.ts
@@ -1,8 +1,17 @@
+import { resolve } from "path";
 import { defineConfig } from 'vite';
 
 export default defineConfig({
   root: '.',
   build: {
-    outDir: 'dist'
+    outDir: 'dist',
+    rollupOptions: {
+      input: {
+        main: resolve(__dirname, "index.html"),
+        zoos: resolve(__dirname, "pages/zoos/zoos.html"),
+        map: resolve(__dirname, "pages/map/map.html"),
+        contact: resolve(__dirname, "pages/contact_us/contact.html")
+      }
+    }
   }
 });
